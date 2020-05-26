45b226c0d766c83c308f715164dc4c285b6d4966
Kotaro Sonoda <sonoda@cis.nagasaki-u.ac.jp>
2020-05-26 21:45:36 +0900

    some fix

--- sample_codes/dist_sample2.c
+++ sample_codes/dist_sample2.c
@@ -5,75 +5,93 @@
 #include <stdlib.h>
 #include <math.h>
 
-#define DIM 784
+#define NumImg 100
+#define SizeRow 28
+#define SizeCol 28
+#define SizeBuff 128
+#define LenFilename 100
+#define DIM (SizeRow * SizeCol)
+#define WHITE 255
+#define RefImgNo 1
 
 int main(void)
 {
-    int i,j,Image;                              //■ ループ用変数
-    unsigned char Pix[100][28][28];             //■ バイナリ画像データ格納用
-    char buff[128];
-    char filename[100];                         //■ ファイルネーム格納用
-    FILE *fp;                                   //■ ファイルポインタ
-    double fsum, Pvec[100][DIM];
-    int tes_label[100];
+	int iRow, iCol, iDim, iImg;					 //■ ループ用変数
+	unsigned char Pix[NumImg][SizeRow][SizeCol]; //■ バイナリ画像データ格納用
+	char buff[SizeBuff];
+	char filename[LenFilename]; //■ ファイルネーム格納用
+	FILE *fp;					//■ ファイルポインタ
+	double fsum, Pvec[NumImg][DIM];
+	int tes_label[NumImg];
 
-    for(Image=0;Image<100;Image++){
-    
-	/* sprintf関数でファイル名を作成する */
-	sprintf(filename,"./test_data/d%d.pgm",Image);
-	//	printf("画像 %sを開きます\n",filename);
-    
-	fp=fopen(filename, "rb" );            //■ ファイルを開く
-	fgets(buff,128,fp);                   //■ ファイルの識別符号を読み込み
-	fgets(buff,128,fp);                   //■ 画像サイズの読み込み
-	fgets(buff,128,fp);                   //■ 最大輝度値の読み込み
-    
-	//■ 画像データの読み込み
-	for(i=0;i<28;i++){
-	    for(j=0;j<28;j++){
-		Pix[Image][i][j] = fgetc(fp);
-	    }
+	for (iImg = 0; iImg < NumImg; iImg++)
+	{
+
+		/* sprintf関数でファイル名を作成する */
+		sprintf(filename, "./test_data/d%d.pgm", iImg);
+		//	printf("画像 %sを開きます\n",filename);
+
+		fp = fopen(filename, "rb");	   //■ ファイルを開く
+		fgets(buff, sizeof(buff), fp); //■ ファイルの識別符号を読み込み
+		fgets(buff, sizeof(buff), fp); //■ 画像サイズの読み込み
+		fgets(buff, sizeof(buff), fp); //■ 最大輝度値の読み込み
+
+		//■ 画像データの読み込み
+		for (iRow = 0; iRow < SizeRow; iRow++)
+		{
+			for (iCol = 0; iCol < SizeCol; iCol++)
+			{
+				Pix[iImg][iRow][iCol] = fgetc(fp);
+			}
+		}
+		fclose(fp);
 	}
-	fclose(fp);
-    }
 
-    //■ ラベルデータの読み込み
-    fp=fopen("test_label.txt", "rb" );
-    for(i=0;i<100;i++){
-	fgets(buff,128,fp);
-	sscanf(buff,"%d",&tes_label[i]);
-    }
-
-    //■ 画素値を変換(白:0 黒:1)して，１次元ベクトルに代入
-    for(Image=0;Image<100;Image++){
-	for(i=0;i<28;i++){
-	    for(j=0;j<28;j++){
-		Pvec[Image][28*i+j] = (255 - Pix[Image][i][j]) / 255.0;
-	    }
+	//■ ラベルデータの読み込み
+	fp = fopen("test_label.txt", "rb");
+	for (iImg = 0; iImg < NumImg; iImg++)
+	{
+		fgets(buff, sizeof(buff), fp);
+		sscanf(buff, "%d", &tes_label[iImg]);
 	}
-    }
 
-    //■ ベクトルの要素の和を 1に正規化
-    for(Image=0;Image<100;Image++){
-	fsum = 0;
-	for(i=0;i<DIM;i++){
-	    fsum += Pvec[Image][i];
+	//■ 画素値を変換(白:0 黒:1)して，１次元ベクトルに代入
+	for (iImg = 0; iImg < NumImg; iImg++)
+	{
+		for (iRow = 0; iRow < SizeRow; iRow++)
+		{
+			for (iCol = 0; iCol < SizeCol; iCol++)
+			{
+				Pvec[iImg][SizeCol * iRow + iCol] = (WHITE - (double)Pix[iImg][iRow][iCol]) / WHITE;
+			}
+		}
 	}
-	for(i=0;i<DIM;i++){
-	    Pvec[Image][i] /= fsum;
-	}
-    }
 
-    //■ １枚目の画像とそれ以外の画像の間の距離を計算
-    for(Image=0;Image<100;Image++){
-	fsum=0;
-	for(i=0;i<DIM;i++){
-	    fsum += (Pvec[0][i]-Pvec[Image][i])
-		*(Pvec[0][i]-Pvec[Image][i]);
+	//■ ベクトルの要素の和を 1に正規化
+	for (iImg = 0; iImg < NumImg; iImg++)
+	{
+		fsum = 0;
+		for (iDim = 0; iDim < DIM; iDim++)
+		{
+			fsum += Pvec[iImg][iDim];
+		}
+		for (iDim = 0; iDim < DIM; iDim++)
+		{
+			Pvec[iImg][iDim] /= fsum;
+		}
 	}
-	printf("No.%d  label:%d  dist:%.4f\n", 
-	       Image+1, tes_label[Image], sqrt(fsum) );
-    }
 
-    return(0);
+	//■ １枚目の画像とそれ以外の画像の間の距離を計算
+	for (iImg = 0; iImg < NumImg; iImg++)
+	{
+		fsum = 0;
+		for (iDim = 0; iDim < DIM; iDim++)
+		{
+			fsum += (Pvec[RefImgNo - 1][iDim] - Pvec[iImg][iDim]) * (Pvec[RefImgNo - 1][iDim] - Pvec[iImg][iDim]);
+		}
+		printf("No.%d  label:%d  dist:%.4f\n",
+			   iImg + 1, tes_label[iImg], sqrt(fsum));
+	}
+
+	return 0;
 }
